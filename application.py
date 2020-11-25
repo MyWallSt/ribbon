2e8d20d8f71b0ffdf639a30873ef98fdd05ce88e
Sammy <sammy.odenhoven@rubicoin.com>
2020-11-25 08:12:28 +0000

    TEST - application.py with routes in it,

--- application.py
+++ application.py
@@ -1,4 +1,22 @@
-from application import application, routes
+# from application import application, routes
+
+# if __name__ == '__main__':
+#     application.run(host='0.0.0.0')
+
+
+from flask import Flask, render_template, request
+
+# Elastic Beanstalk initalization
+application = Flask(__name__)
+application.debug=True
+# change this to your own value
+application.secret_key = 'cC1YCIWOj9GgWspgNEo2'   
+
+@application.route('/', methods=['GET', 'POST'])
+@application.route('/index', methods=['GET', 'POST'])
+def index():       
+    
+    return render_template('welcome.html')
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
